7fb73d638fa13938c29bf42822135400c27b8d30
calvix <vaclav@giantswarm.io>
2020-06-04 14:40:29 +0200

    add-migrator-code-base

---
+++ migrator/etcd.go
@@ -0,0 +1,60 @@
+package migrator
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+
+	"github.com/giantswarm/microerror"
+	etcdclientv3 "go.etcd.io/etcd/clientv3"
+)
+
+const (
+	dialTimeout = time.Minute
+)
+
+func createEtcdClient(caFile string, certFile string, keyFile string, endpoint string) (*etcdclientv3.Client, error) {
+	etcdCertPair, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	etcdCaCert, err := CertPoolFromFile(caFile)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	config := etcdclientv3.Config{
+		DialTimeout: dialTimeout,
+		Endpoints: []string{
+			endpoint,
+		},
+
+		TLS: &tls.Config{
+			Certificates:       []tls.Certificate{etcdCertPair},
+			ClientCAs:          etcdCaCert,
+			RootCAs:            etcdCaCert,
+			InsecureSkipVerify: false,
+		},
+	}
+
+	client, err := etcdclientv3.New(config)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return client, nil
+}
+
+func etcdPeerName(index int, baseDomain string) string {
+	return fmt.Sprintf("https://etcd%d.%s:2380", index, baseDomain)
+}
+
+func initialCluster(startingIndex int, baseDomain string, nodesCount int) string {
+	r := fmt.Sprintf("etcd%d=https:\\/\\/etcd%d.%s:2380", startingIndex, startingIndex, baseDomain)
+
+	for i := 1; i < nodesCount; i++ {
+		r += fmt.Sprintf(",etcd%d=https:\\/\\/etcd%d.%s:2380", startingIndex+i, startingIndex+i, baseDomain)
+	}
+	return r
+}
---
+++ migrator/k8s.go
@@ -0,0 +1,44 @@
+package migrator
+
+import (
+	"sort"
+
+	"github.com/giantswarm/microerror"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+const (
+	labelMasterID = "giantswarm.io/master-id"
+)
+
+func createK8SClient() (kubernetes.Interface, error) {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	return clientset, nil
+}
+
+// getNodeNames return nodeName list ordered by master id label.
+func getNodeNames(nodes []v1.Node) []string {
+	// sort nodes by masterID
+	sort.Slice(nodes, func(i int, j int) bool {
+		return nodes[i].Labels[labelMasterID] < nodes[j].Labels[labelMasterID]
+	})
+
+	list := []string{
+		nodes[0].Name,
+		nodes[1].Name,
+		nodes[2].Name,
+	}
+	return list
+}
